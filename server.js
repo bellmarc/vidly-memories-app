a01d19edbcaf32cf8048d3aeaeef613c49764e88
Marcielo Bell <marcielobell@gmail.com>
2020-01-13 18:57:03 -0800

    Deploy version

--- server.js
+++ server.js
@@ -2,15 +2,17 @@ const express = require("express");
 const app = express();
 require("dotenv").config();
 const morgan = require("morgan");
+const path = require("path");
 const mongoose = require("mongoose");
 const PORT = process.env.PORT || 5000;
 const expressJwt = require("express-jwt");
 
 app.use(express.json());
 app.use(morgan("dev"));
+app.use(express.static(path.join(__dirname, "client", "build")));
 
 mongoose.connect(
-  "mongodb://localhost:27017/vidly",
+  process.env.MONGODBI_URI || "mongodb://localhost:27017/vidly",
   {
     useNewUrlParser: true,
     useFindAndModify: true,
@@ -25,7 +27,7 @@ app.use("/auth", require("./routes/authRouter.js"));
 app.use("/api", expressJwt({ secret: process.env.SECRET }));
 app.use("/api/user", require("./routes/userRouter.js"));
 app.use("/api/booking", require("./routes/bookingRouter.js"));
-app.use("/location", require("./routes/locationRouter.js"))
+app.use("/location", require("./routes/locationRouter.js"));
 
 //Error Handler
 app.use((err, req, res, next) => {
@@ -34,6 +36,10 @@ app.use((err, req, res, next) => {
     res.status(err.status);
   }
   return res.send({ errMsg: err.message });
+});
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "client", "build", "index.html"));
 });
 
 app.listen(PORT, () => {
